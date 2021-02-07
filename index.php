50f9b71d0218de281c84112021364294b7228ee2
Mehrdad <amini_pk@hotmail.com>
2021-02-07 22:09:22 +0100

    created formed structure

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Todo List</title>
+</head>
+<body>
+    <div class="">
+        <div class="">
+        <h3>Add a New Task</h3>
+
+        <form action="" method="POST">
+                <div class="">
+                    <input class="" type="text" name="" placeholder="">
+                </div>
+
+                <div class="">
+                    <input class="" value=""
+                    type="submit">
+                </div>
+            </form>
+        </div>
+        <div class="">
+            <table class="">
+                <thead>
+                    <th>ID</th>
+                    <th>Todo</th>
+                    <th>Date Added</th>
+                    <th>Edit Todo</th>
+                    <th>Delete Todo</th>
+                </thead>
+</body>
+</html>
